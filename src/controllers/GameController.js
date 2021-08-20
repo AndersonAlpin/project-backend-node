7be4818c042171000bf30c6700f15c6d211504ff
AndersonAlpin <andersonalpin@hotmail.com>
2021-08-20 00:22:59 -0300

    Retorna todos os games
    Este commit utiliza o axios para buscar todos os games na api da steam,
    sem a necessidade de fazer a conversão dos dados para JSON.

--- src/controllers/GameController.js
+++ src/controllers/GameController.js
@@ -1,6 +1,17 @@
+const axios = require("axios");
+const URL_ALL_GAMES =
+  "https://api.steampowered.com/ISteamApps/GetAppList/v0002/?format=json";
+
 class GameController {
   async getAll(req, res) {
-    res.json({ result: "OK" });
+    axios
+      .get(URL_ALL_GAMES)
+      .then((data) => {
+        res.send(data.data.applist.apps);
+      })
+      .catch((error) => {
+        res.json({ error });
+      });
   }
 }
 
