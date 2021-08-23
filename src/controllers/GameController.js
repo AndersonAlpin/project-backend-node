72327781cde8de42f383915ed0723e369116211c
AndersonAlpin <andersonalpin@hotmail.com>
2021-08-22 23:49:49 -0300

    Reorganiza validações
    Este commit adiciona e reorganiza as validações para que informe todos os dados indevidos de uma só vez.

--- src/controllers/GameController.js
+++ src/controllers/GameController.js
@@ -6,6 +6,8 @@ const Game = require("../models/Game");
 const URL_ALL_GAMES =
   "https://api.steampowered.com/ISteamApps/GetAppList/v0002/?format=json";
 const URL_ONE_GAME = "https://store.steampowered.com/api/appdetails?appids=";
+const emailRegex =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
 class GameController {
   async getAll(req, res) {
@@ -39,13 +41,40 @@ class GameController {
   }
 
   async addFavorite(req, res) {
-    try {
-      let { appid, rating } = req.body;
-      let { email, password } = req.headers;
-      let isPasswordRight = "";
+    let { appid, rating } = req.body;
+    let { email, password } = req.headers;
+    let isPasswordRight = "";
+    let errors = [];
 
-      // VALIDAÇÃO DE USUÁRIO
+    // VALIDAÇÕES
+    if (isNaN(appid)) {
+      errors.push({ appid: "O campo appid precisa ser um número" });
+    }
+
+    if (!rating || rating < 0 || rating > 5) {
+      errors.push({ rating: "A nota deve ser um valor entre 0 e 5." });
+    }
+
+    if (!emailRegex.test(email)) {
+      errors.push({ email: "Digite um email válido." });
+    }
+
+    if (password.length < 6) {
+      errors.push({ password: "A senha precisa ter no mínimo 6 dígitos." });
+    }
+
+    if (errors.length > 0) {
+      return res.json({ errors });
+    }
+
+    try {
+      // BUSCA DE UM USUÁRIO
       let user = await User.findOne({ email });
+
+      if (!user) {
+        errors.push({ email: "Este email não existe." });
+        return res.json({ errors });
+      }
 
       if (user) {
         isPasswordRight = await bcrypt.compare(password, user.password);
@@ -60,25 +89,13 @@ class GameController {
 
       // VERIFICA SE O FAVORITO JÁ ESTÁ CADASTRADO
       let gameFavorite = await Game.findOne({ appid });
-      if (gameFavorite) {
+      if (user && gameFavorite) {
         return res
           .status(409)
           .send({ message: "Este jogo já esta salvo nos favoritos." });
       }
 
       // BUSCA UM GAME NO SERVIDOR
-      if (isNaN(appid)) {
-        return res
-          .status(400)
-          .send({ message: "Por favor, insira um ID válido." });
-      }
-
-      if (rating < 0 || rating > 5) {
-        return res
-          .status(406)
-          .send({ msg: "Por favor, insira uma nota entre 0 e 5." });
-      }
-
       let data = await axios.get(`${URL_ONE_GAME}${appid}`);
       let game = data.data;
       game.rating = rating;
@@ -102,17 +119,38 @@ class GameController {
       let newFavorite = await Game.create(game);
       return res.json({ newFavorite });
     } catch (error) {
-      return res.status(500).send({ error: "Erro interno no servidor." });
+      return res
+        .status(500)
+        .send({ error: "Erro na comunicação com o banco de dados." });
     }
   }
 
   async getFavorites(req, res) {
-    try {
-      let { email, password } = req.headers;
-      let isPasswordRight = "";
+    let { email, password } = req.headers;
+    let isPasswordRight = "";
+    let errors = [];
 
-      // VALIDAÇÃO DE USUÁRIO
+    // VALIDAÇÕES
+    if (!emailRegex.test(email)) {
+      errors.push({ email: "Digite um email válido." });
+    }
+
+    if (password.length < 6) {
+      errors.push({ password: "A senha precisa ter no mínimo 6 dígitos." });
+    }
+
+    if (errors.length > 0) {
+      return res.json({ errors });
+    }
+
+    try {
+      // BUSCA DE UM USUÁRIO
       let user = await User.findOne({ email });
+
+      if (!user) {
+        errors.push({ email: "Este email não existe." });
+        return res.json({ errors });
+      }
 
       if (user) {
         isPasswordRight = await bcrypt.compare(password, user.password);
@@ -129,24 +167,43 @@ class GameController {
       let favorites = await Game.find({ user_id: user._id });
       return res.json({ favorites });
     } catch (error) {
-      return res.status(500).send({ error: "Erro interno no servidor." });
+      return res
+        .status(500)
+        .send({ error: "Erro na comunicação com o banco de dados." });
     }
   }
 
   async deleteFavorite(req, res) {
+    let { email, password } = req.headers;
+    let appid = req.params.appid;
+    let isPasswordRight = "";
+    let errors = [];
+
+    // VALIDAÇÕES
+    if (isNaN(appid)) {
+      errors.push({ appid: "O campo appid precisa ser um número" });
+    }
+
+    if (!emailRegex.test(email)) {
+      errors.push({ email: "Digite um email válido." });
+    }
+
+    if (password.length < 6) {
+      errors.push({ password: "A senha precisa ter no mínimo 6 dígitos." });
+    }
+
+    if (errors.length > 0) {
+      return res.json({ errors });
+    }
+
     try {
-      let appid = req.params.appid;
-      let { email, password } = req.headers;
-      let isPasswordRight = "";
-
-      if (isNaN(appid)) {
-        return res
-          .status(400)
-          .send({ message: "Por favor, insira um ID válido." });
-      }
-
-      // VALIDAÇÃO DE USUÁRIO
+      // BUSCA DE UM USUÁRIO
       let user = await User.findOne({ email });
+
+      if (!user) {
+        errors.push({ email: "Este email não existe." });
+        return res.json({ errors });
+      }
 
       if (user) {
         isPasswordRight = await bcrypt.compare(password, user.password);
@@ -168,7 +225,9 @@ class GameController {
       await Game.deleteOne(gameFavorite);
       return res.send({ message: "Jogo deletado com sucesso." });
     } catch (error) {
-      return res.status(500).send({ error: "Erro interno no servidor." });
+      return res
+        .status(500)
+        .send({ error: "Erro na comunicação com o banco de dados." });
     }
   }
 }
